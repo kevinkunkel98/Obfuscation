3118ca1b6cd8cbac2c45f3150edd6e6521e3413c
kevinkunkel98 <kevinkunkel98@gmail.com>
2024-12-23 13:50:17 +0100

    add game panel

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,7 +2,7 @@ package main;
 
 import javax.swing.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     final int originalTileSize = 16;
     final int scale = 3;
@@ -13,10 +13,24 @@ public class GamePanel extends JPanel {
     final int screenWidth = maxScreenCol * tileSize;
     final int screenHeight = maxScreenRow * tileSize;
 
-    public GamePanel() {
+    Thread gameThread;
 
+    public GamePanel() {
         this.setPreferredSize(new java.awt.Dimension(screenWidth, screenHeight));
         this.setBackground(java.awt.Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while (gameThread != null) {
+            System.out.println("Game loop is running");
+            // Add game loop logic here (e.g., update game state, render game frame)
+        }
     }
 }
